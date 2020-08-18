8b05e5f40f5127de63cfce285ad57359588035e0
David Boedigheimer <stealth259@gmail.com>
2020-08-18 15:44:27 -0700

    Add start of first tests.

--- PierresBakeryTests/Models/PierresBakeryTests.cs
+++ PierresBakeryTests/Models/PierresBakeryTests.cs
@@ -1,14 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System;
 
 namespace PierresBakeryTests 
 {
     [TestClass]
-    public class BreadPastryTests 
+    public class BakeryBreadTests 
     {
         // Test methods go here
         [TestMethod]
-        public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue () 
+        public void BreadSetPrice_DisplaysBreadSetPrice_Int () 
+        {
+            // Arrange
+            int bread = "5";
+            
+            // Assert.AreEqual (EXPECTED RESULT, CODE TO TEST);
+        }
+    }
+
+    [TestClass]
+    public class BakeryPastryTests 
+    {
+        // Test methods go here
+        [TestMethod]
+        public void PastrySetPrice_DisplaysPastrySetPrice_Int () 
         {
             // any necessary logic to prep for test; instantiating new classes, etc.
             // Assert.AreEqual (EXPECTED RESULT, CODE TO TEST);
