4347cbb041e32f09d4d2aa4ac467d3544de85566
David Boedigheimer <stealth259@gmail.com>
2020-08-25 16:09:18 -0700

    Add start of the main class for the terminal.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,7 +8,23 @@ namespace PierresBakery
     {
         public static void Main()
         {
-
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Bread is $5 per loaf.");
+            Console.WriteLine("Pastrys are $2 each.");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("We have 2 deals:");
+            Console.WriteLine("Bread: Buy 2 get 1 free");
+            Console.WriteLine("Pastry: Buy 3 for $5");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Which would you like to order: [b for Bread / p for Pastry]");
+            string type = Console.ReadLine().ToLower();
+            if (type == "b")
+            {
+                Console.WriteLine("How many do you want to order?");
+                Console.ReadLine();
+                Bread userBread = new Bread();
+            }
         }
     }
 }
